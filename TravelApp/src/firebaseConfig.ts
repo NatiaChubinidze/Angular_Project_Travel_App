7c1e6f8b0d5574a4ee914d3b9af45c3021cb266b
LENOVO <chubinidze.natia97@gmail.com>
2021-04-05 16:32:29 +0400

    firebase-auth-and-form-validation

---
+++ TravelApp/src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyZ9CJESc_3iMu_tucQnoy4eu7uH3zfNg",
+    authDomain: "travelapp-437f8.firebaseapp.com",
+    projectId: "travelapp-437f8",
+    storageBucket: "travelapp-437f8.appspot.com",
+    messagingSenderId: "814244893519",
+    appId: "1:814244893519:web:16d19df951bf64fc1eeb3a",
+    measurementId: "G-4JWT1Y191X"
+  };
