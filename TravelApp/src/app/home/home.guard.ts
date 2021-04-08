d1fab826cd9cd321b62a0689f8ec06b3d9cc3dec
LENOVO <chubinidze.natia97@gmail.com>
2021-04-08 10:32:55 +0400

    update:refactoring and validations and guards

--- TravelApp/src/app/home/home.guard.ts
+++ TravelApp/src/app/home/home.guard.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { FireBaseAuthService } from './firebase-auth.service';
+import { FireBaseAuthService } from '../auth/firebase-auth.service';
 
-import {LocationService} from '../destination/location.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  constructor(private firebaseAuth:FireBaseAuthService, private locationService:LocationService){}
+export class HomeGuard implements CanActivate {
+  constructor(private firebaseAuth:FireBaseAuthService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.firebaseAuth.userIsActive() && this.locationService.dataIsValid;
+    return this.firebaseAuth.userIsLoggedIn;
   }
   
 }
